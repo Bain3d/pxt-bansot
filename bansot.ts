84bf02ab8e658e8c0b46c7b8eec835f05ffbdae7
限速53公里 <chacharo@163.com>
2021-03-04 11:20:48 +0800

    Update bansot.ts
    ADD PS Controller

--- bansot.ts
+++ bansot.ts
@@ -505,3 +505,192 @@ namespace bansot {
     }
 }
 
+//% color="#31C7D5" weight=10 icon="\uf1d1"  block="PS手柄"
+namespace ps2controller {
+    //**Reverse blocks -----
+    const rbits = hex`
+    008040C020A060E0109050D030B070F0088848C828A868E8189858D838B878F8
+    048444C424A464E4149454D434B474F40C8C4CCC2CAC6CEC1C9C5CDC3CBC7CFC
+    028242C222A262E2129252D232B272F20A8A4ACA2AAA6AEA1A9A5ADA3ABA7AFA
+    068646C626A666E6169656D636B676F60E8E4ECE2EAE6EEE1E9E5EDE3EBE7EFE
+    018141C121A161E1119151D131B171F1098949C929A969E9199959D939B979F9
+    058545C525A565E5159555D535B575F50D8D4DCD2DAD6DED1D9D5DDD3DBD7DFD
+    038343C323A363E3139353D333B373F30B8B4BCB2BAB6BEB1B9B5BDB3BBB7BFB
+    078747C727A767E7179757D737B777F70F8F4FCF2FAF6FEF1F9F5FDF3FBF7FFF`
+
+    function rbit(value: number): number {
+        return rbits[value] || 0x00;
+    }
+
+    function rbuffer(b: Buffer): Buffer {
+        let output = pins.createBuffer(b.length);
+        for (let i = 0; i < b.length; i++) {
+            let n = b[i]
+            output[i] = rbit(n)
+        }
+        return output
+    }
+    //**Reverse blocks -----
+
+    let chipSelect = DigitalPin.P12
+    pins.digitalWritePin(chipSelect, 1)
+
+    pins.spiPins(DigitalPin.P15, DigitalPin.P14, DigitalPin.P13)
+    pins.spiFormat(8, 3)
+    pins.spiFrequency(250000)
+
+    let pad = pins.createBuffer(6)
+    let connected = false
+
+    const poll_cmd = hex
+        `014200000000000000`
+
+    function send_command(transmit: Buffer): Buffer {
+        // 处理位顺序
+        transmit = rbuffer(transmit)
+
+        let receive = pins.createBuffer(transmit.length);
+
+        pins.digitalWritePin(chipSelect, 0);
+        // 实际发送命令
+        for (let i = 0; i < transmit.length; i++) {
+            receive[i] = pins.spiWrite(transmit[i]);
+        }
+        pins.digitalWritePin(chipSelect, 1)
+
+        // 处理位顺序
+        receive = rbuffer(receive)
+
+        return receive
+    }
+
+    export enum PS2Button {
+        //% blockId="Left" block="向左方向键"
+        Left,
+        //% blockId="Down" block="向下方向键"
+        Down,
+        //% blockId="Right" block="向右方向键"
+        Right,
+        //% blockId="Up" block="向上方向键"
+        Up,
+        //% blockId="Start" block="开始(Start)按键"
+        Start,
+        //% blockId="Analog_Left" block="右侧摇杆按下"
+        Analog_Left,
+        //% blockId="Analog_Right" block="左侧摇杆按下"
+        Analog_Right,
+        //% blockId="Select" block="选择(Select)按键"
+        Select,
+        //% blockId="Square" block="正方形(□)按键"
+        Square,
+        //% blockId="Cross" block="叉型(×)按键"
+        Cross,
+        //% blockId="Circle" block="圆型(○)按键"
+        Circle,
+        //% blockId="Triangle" block="三角形(△)按键"
+        Triangle,
+        //% blockId="R1" block="R1按键"
+        R1,
+        //% blockId="L1" block="L1按键"
+        L1,
+        //% blockId="R2" block="R2按键"
+        R2,
+        //% blockId="L2" block="L2按键"
+        L2,
+        // //% blockId="Buttons" block="按键(空缺)"
+        // Buttons,
+    };
+    export enum PS2Analog {
+        //% blockId="RX" block="右侧摇杆X"
+        RX,
+        //% blockId="RY" block="右侧摇杆Y"
+        RY,
+        //% blockId="LX" block="左侧摇杆x"
+        LX,
+        //% blockId="LY" block="左侧摇杆Y"
+        LY,
+    }
+
+    //% blockId=bansot_button_pressed block="手柄|%b|按下"
+    //% weight=99
+    //% blockGap=50
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function button_pressed(b: PS2Button): boolean {
+        poll();
+        if (!connected) return false
+        switch (b) {
+            case PS2Button.Left:
+                return pad[0] & 0x80 ? false : true;
+            case PS2Button.Down:
+                return pad[0] & 0x40 ? false : true;
+            case PS2Button.Right:
+                return pad[0] & 0x20 ? false : true;
+            case PS2Button.Up:
+                return pad[0] & 0x10 ? false : true;
+            case PS2Button.Start:
+                return pad[0] & 0x08 ? false : true;
+            case PS2Button.Analog_Left:
+                return pad[0] & 0x04 ? false : true;
+            case PS2Button.Analog_Right:
+                return pad[0] & 0x02 ? false : true;
+            case PS2Button.Select:
+                return pad[0] & 0x01 ? false : true;
+            case PS2Button.Square:
+                return pad[1] & 0x80 ? false : true;
+            case PS2Button.Cross:
+                return pad[1] & 0x40 ? false : true;
+            case PS2Button.Circle:
+                return pad[1] & 0x20 ? false : true;
+            case PS2Button.Triangle:
+                return pad[1] & 0x10 ? false : true;
+            case PS2Button.R1:
+                return pad[1] & 0x08 ? false : true;
+            case PS2Button.L1:
+                return pad[1] & 0x04 ? false : true;
+            case PS2Button.R2:
+                return pad[1] & 0x02 ? false : true;
+            case PS2Button.L2:
+                return pad[1] & 0x01 ? false : true;
+            // case PS2Button.Buttons:
+            //     return ~((pad[1] << 8) | pad[0]) & 0xffff;
+        }
+        return false;
+    }
+    //% blockId=bansot_analog_value block="手柄摇杆|%b|的值"
+    //% weight=99
+    //% blockGap=50
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function analogValue(b: PS2Analog): number {
+        poll();
+        if (!connected) return 0x00
+        switch (b) {
+            case PS2Analog.RX:
+                return pad[2] - 0x80;
+            case PS2Analog.RY:
+                return pad[3] - 0x80;
+            case PS2Analog.LX:
+                return pad[4] - 0x80;
+            case PS2Analog.LY:
+                return pad[5] - 0x80;
+        }
+        return 0;
+    }
+
+    function poll(): boolean {
+        let buf = send_command(poll_cmd)
+        if (buf[2] != 0x5a) {
+            return false;
+        }
+
+        for (let i = 0; i < 6; i++) {
+            pad[i] = buf[3 + i];
+        }
+        connected = true
+        return true
+    }
+
+    // basic.forever(function () {
+    //     // poll();
+    // })
+}
+
